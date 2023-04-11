20925285fc915056eea84e48e9ff9a014a835b5e
Ssd3600 <89728489+Ssd3600@users.noreply.github.com>
2023-04-12 00:06:38 +0700

    update signal

--- app/src/main/java/com/tenk/a1dl_test/information_signal/TrafficSignalFragment.java
+++ app/src/main/java/com/tenk/a1dl_test/information_signal/TrafficSignalFragment.java
@@ -1,4 +1,4 @@
-package com.tenk.a1dl_test;
+package com.tenk.a1dl_test.information_signal;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -7,6 +7,8 @@ import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+
+import com.tenk.a1dl_test.R;
 
 public class TrafficSignalFragment extends Fragment {
 
